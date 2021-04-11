9e412d215c006edeb2bcab477e2f82e6bf4354f5
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2021-04-12 04:13:42 +0500

    public interface car

---
+++ 01-Basic-Exception-Interface/Car.java
@@ -0,0 +1,11 @@
+interface Car {
+    public void display();
+
+    public void setColor(String color);
+    public void setModel(String model);
+    public void setRegNo(int regNo);
+
+    public String getColor();
+    public String getModel();
+    public int getRegNo();
+}
