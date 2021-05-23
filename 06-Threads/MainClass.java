577a279a8b54f25a11adf497926f53f577499ca3
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2021-05-23 05:31:43 +0500

    MainClass for threads runnable

--- 06-Threads/MainClass.java
+++ 06-Threads/MainClass.java
@@ -3,11 +3,9 @@ public class MainClass {
     public static void main(String[] args) {
         ThreadInterface obj1 = new ThreadInterface();
         ThreadInterface obj2 = new ThreadInterface();
-        ThreadInterface obj3 = new ThreadInterface();
 
         obj1.start();
         obj2.start();
-        obj3.start();
 
         // ThreadInterface obj = new ThreadInterface();
         
