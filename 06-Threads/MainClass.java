91d4de25fb725e7b8803e864a891e50802ba22a8
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2021-05-23 07:57:03 +0500

    MainClass of runnable thread (start, join)

--- 06-Threads/MainClass.java
+++ 06-Threads/MainClass.java
@@ -1,15 +1,21 @@
 
-public class MainClass {
-    public static void main(String[] args) {
+public class MainClass{
+    public static void main(String[] args) throws InterruptedException {
         ThreadInterface obj1 = new ThreadInterface();
         ThreadInterface obj2 = new ThreadInterface();
+        ThreadInterface obj3 = new ThreadInterface();
+
+        obj1.setName("thread-1");
+        obj2.setName("thread-2");
+        obj3.setName("thread-3");
 
         obj1.start();
         obj2.start();
-
-        // ThreadInterface obj = new ThreadInterface();
+        obj3.start();
         
-        // Thread thread = new Thread(obj);
-        // thread.run();
+        obj1.join();
+
+        System.out.println("obj3 status: "+obj3.isAlive());
+
     }
 }
