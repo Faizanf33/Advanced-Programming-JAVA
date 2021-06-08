36302bff4d0aa5260565f668b6a9d15fa85ab213
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2021-06-08 09:18:25 +0500

    Main class for Threads

--- 06-Threads/MainClass.java
+++ 06-Threads/MainClass.java
@@ -2,47 +2,51 @@ public class MainClass{
     static int sum = 0;
 
     public static void main(String[] args) throws InterruptedException {
-        ThreadInterface obj1 = new ThreadInterface();
-        ThreadInterface obj2 = new ThreadInterface();
-        ThreadInterface obj3 = new ThreadInterface();
+        // ThreadInterface obj1 = new ThreadInterface();
+        // ThreadInterface obj2 = new ThreadInterface();
+        // ThreadInterface obj3 = new ThreadInterface();
 
-        obj1.setName("thread-1");
-        obj2.setName("thread-2");
-        obj3.setName("thread-3");
+        // obj1.setName("thread-1");
+        // obj2.setName("thread-2");
+        // obj3.setName("thread-3");
 
-        obj1.start();
-        obj2.start();
-        obj3.start();
+        // obj1.start();
+        // obj2.start();
+        // obj3.start();
         
-        obj1.join();
-        
-        Runnable runnable = ()->{
-            for (int i = 1; i <= 10; i++) {
-                sum += i;                
-            }        // state obj = new state();
-            // obj.st = true;
-            // obj.position = new double[]{71.199, 123.21};
-            
-            
-            // // save to file
-            // try {
-            //     String fileString = "output.txt";
-            //     ObjectOutputStream objOutputStream = new ObjectOutputStream(new FileOutputStream(fileString));
-            //     objOutputStream.writeObject(obj);
-            //     objOutputStream.close();
-    
-            // } catch (Exception e) {
-            //     System.err.println(e);
-            // }
-    
+        // obj1.join();
+        int ID = 751998;
+        BankAccount account = new BankAccount(ID);        
+
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    account.deposit(1);
+                    System.out.println("D-Balance: "+account.getBalance());                    
+                }
+
+            }
         };
-        
-        Thread thread = new Thread(runnable);
-        thread.start();
-        // Run with and without 'join' to see the difference
-        thread.join();
 
-        System.out.println("Sum: "+sum);
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                for (double i = 0; i < 10; i++) {
+                    account.withdraw(1);
+                    System.out.println("W-Balance: "+account.getBalance());                    
+                }
+            }
+        };
+
+        // Run with and without 'join' to see the difference
+        t1.setName("1-Thread"+ID);
+        t2.setName("2-Thread"+ID);
+
+        t1.start();
+        t2.start();
+
+        // System.out.println("Balance: "+account.getBalance());
 
     }
 }
